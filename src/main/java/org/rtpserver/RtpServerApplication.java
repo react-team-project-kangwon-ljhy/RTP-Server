2e58d248084f4b11ec3f408f33a625d01f8fc87d
이재현 <2023050@bssm.hs.kr>
2024-10-11 08:29:42 +0900

    추가 : 프로젝트 생성

---
+++ src/main/java/org/rtpserver/RtpServerApplication.java
@@ -0,0 +1,13 @@
+package org.rtpserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtpServerApplication.class, args);
+	}
+
+}
