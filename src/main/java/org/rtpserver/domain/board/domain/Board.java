33662b79346435c856ebc68ed66e379a11541b38
ljhy07 <ij184399@gmail.com>
2025-02-07 23:14:33 +0900

    add(board): 추가적인 서비스 로직으로 생성자와 함수 추가

--- src/main/java/org/rtpserver/domain/board/domain/Board.java
+++ src/main/java/org/rtpserver/domain/board/domain/Board.java
@@ -2,20 +2,29 @@ package org.rtpserver.domain.board.domain;
 
 import io.hypersistence.utils.hibernate.type.array.StringArrayType;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Type;
+import org.rtpserver.domain.user.domain.Users;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.List;
 
+@Getter
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Board {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "users_id")
-//    private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId")
+    private Users user;
 
     @Column(nullable = false, length = 200)
     private String title;
@@ -30,5 +39,18 @@ public class Board {
 //    @Type(StringArrayType.class)
 //    @Column(columnDefinition = "varchar[]")
 //    private List<String> imgs;
+
+    public Board(Users user, String title, String contents, List<String> tags) {
+        this.user = user;
+        this.title = title;
+        this.contents = contents;
+        this.tags = tags;
+    }
+
+    public void update(String title, String contents, List<String> tags) {
+        this.title = title;
+        this.contents = contents;
+        this.tags = tags;
+    }
 
 }
