157820e21e87cb313089d39f8c6df72d13d03173
ljhy07 <ij184399@gmail.com>
2025-02-14 23:21:04 +0900

    add(auth): 레포지토리 추가

---
+++ src/main/java/org/rtpserver/domain/auth/domain/repository/RefreshRepository.java
@@ -0,0 +1,9 @@
+package org.rtpserver.domain.auth.domain.repository;
+
+import org.rtpserver.domain.auth.domain.Refresh;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RefreshRepository extends JpaRepository<Refresh, Long> {
+}
