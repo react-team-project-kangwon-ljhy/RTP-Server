b2194fa7700855a7eca6eadb55018f381f9b4ad5
ljhy07 <ij184399@gmail.com>
2025-02-17 20:24:44 +0900

    add(global): swagger 세팅 추가 & 마무리

---
+++ src/main/java/org/rtpserver/global/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package org.rtpserver.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("RTP-Project API") // API의 제목
+                .description("RTP-Project Server v1.0") // API에 대한 설명
+                .version("0.1"); // API의 버전
+    }
+}
